c561cc889c6fd0111d6b7ec662288376a7299952
Alexander <a.k.muller.2@student.rug.nlgit config --global --listgit config --global user.email a.k.muller.2@student.rug.nl>
2024-10-04 19:47:55 +0200

    Done

---
+++ Blandað Best.py
@@ -0,0 +1,8 @@
+if int(input()) == 1:
+    print(str(input()))
+else:
+    meat_1, meat_2 = str(input()), str(input())
+    if meat_1 == meat_2:
+        print(meat_1)
+    else:
+        print("blandad best")
